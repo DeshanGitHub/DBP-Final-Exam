b5ee57fed1ad7accf65e211775c5c707aa6e5281
Kavindu Deshan <kkavindu.deshan@gmail.com>
2022-07-12 10:58:58 +0530

    Added Text Fields and Table to student form

--- src/controller/StudentFormController.java
+++ src/controller/StudentFormController.java
@@ -1,4 +1,12 @@
 package controller;
 
+import com.jfoenix.controls.JFXTextField;
+
 public class StudentFormController {
+    public JFXTextField txtStudentEmail;
+    public JFXTextField txtStudentId;
+    public JFXTextField txtStudentName;
+    public JFXTextField txtStudentContact;
+    public JFXTextField txtStudentAddress;
+    public JFXTextField txtStudentNIC;
 }
